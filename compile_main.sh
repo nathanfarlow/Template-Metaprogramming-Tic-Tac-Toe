313fad9cc7fad96474ddd2d2e45ac74849e25022
Nathan Farlow <natethegreatest11@gmail.com>
2019-10-12 17:41:48 -0500

    Remove whitespace

--- compile_main.sh
+++ compile_main.sh
@@ -1,2 +1,1 @@
-
 /usr/bin/time -v g++ -ftemplate-depth=30000 src/main.cpp src/tictactoe.cpp -o tictactoe 2> main_log.txt
--- compile_tests.sh
+++ compile_tests.sh
@@ -1,2 +1,1 @@
-
 /usr/bin/time -v g++ -ftemplate-depth=30000 src/test.cpp src/tictactoe.cpp 2> test_log.txt
