9057ed74f32fea2bc95fc8d2c01da2403a5d5087
Nathan Farlow <natethegreatest11@gmail.com>
2019-10-12 16:58:13 -0500

    Add compilation scripts

---
+++ compile_main.sh
@@ -0,0 +1,2 @@
+
+/usr/bin/time -v g++ -ftemplate-depth=30000 src/main.cpp src/tictactoe.cpp -o tictactoe 2> main_log.txt
---
+++ compile_tests.sh
@@ -0,0 +1,2 @@
+
+/usr/bin/time -v g++ -ftemplate-depth=30000 src/test.cpp src/tictactoe.cpp 2> test_log.txt
