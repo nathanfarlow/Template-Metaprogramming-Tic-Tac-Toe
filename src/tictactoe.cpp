9381304726470d010788fb975ac2e91dfb073cd0
Nathan Farlow <natethegreatest11@gmail.com>
2019-10-11 20:51:47 -0500

    Fix checking error and add table lookup implementation

--- src/tictactoe.cpp
+++ src/tictactoe.cpp
@@ -1,22 +1,24 @@
 #include "tictactoe.hpp"
 
 #include <array>
+#include <algorithm>
+#include <cmath>
 
 #define abs(a) ((a) >= 0 ? (a) : -1 * (a))
 
 template<size_t a, size_t b>
-struct pow {
-    enum {value = a * pow<a, b - 1>::value};
+struct TemplatePow {
+    enum {value = a * TemplatePow<a, b - 1>::value};
 };
 
 template<size_t a>
-struct pow<a, 0> {
+struct TemplatePow<a, 0> {
     enum {value = 1};
 };
 
 constexpr size_t BOARD_SIDE_LENGTH = 2;
 constexpr size_t BOARD_AREA = BOARD_SIDE_LENGTH * BOARD_SIDE_LENGTH;
-constexpr size_t TABLE_SIZE = pow<3, BOARD_AREA>::value;
+constexpr size_t TABLE_SIZE = TemplatePow<3, BOARD_AREA>::value;
 
 typedef std::array<Evaluation, TABLE_SIZE> LookupTable;
 
@@ -47,24 +49,24 @@ struct PlayerCounter<state, player, BOARD_AREA> {
 };
 
 //Check if a player won down a certain row
-template<size_t state, size_t row, size_t player, size_t col = 0>
+template<size_t state, size_t player, size_t row, size_t col = 0>
 struct CheckRow {
     enum {value = CharHelper<state, row, col>::value == player
-            && CheckRow<state, row, player, col + 1>::value};
+            && CheckRow<state, player, row, col + 1>::value};
 };
-template<size_t state, size_t row, size_t player>
-struct CheckRow<state, row, player, BOARD_SIDE_LENGTH> {
+template<size_t state, size_t player, size_t row>
+struct CheckRow<state, player, row, BOARD_SIDE_LENGTH> {
     enum {value = true};
 };
 
 //Check if a player won down a certain column
-template<size_t state, size_t col, size_t player, size_t row = 0>
+template<size_t state, size_t player, size_t col, size_t row = 0>
 struct CheckColumn {
     enum {value = CharHelper<state, row, col>::value == player
-            && CheckColumn<state, col, player, row + 1>::value};
+            && CheckColumn<state, player, col, row + 1>::value};
 };
-template<size_t state, size_t col, size_t player>
-struct CheckColumn<state, col, player, BOARD_SIDE_LENGTH> {
+template<size_t state, size_t player, size_t col>
+struct CheckColumn<state, player, col, BOARD_SIDE_LENGTH> {
     enum {value = true};
 };
 
@@ -72,13 +74,12 @@ struct CheckColumn<state, col, player, BOARD_SIDE_LENGTH> {
 template<size_t state, size_t player, size_t index = 0>
 struct RowColChecker {
     enum {value = CheckRow<state, player, index>::value
-            && CheckColumn<state, player, index>::value
-            && RowColChecker<state, player, index + 1>::value};
+            || CheckColumn<state, player, index>::value
+            || RowColChecker<state, player, index + 1>::value};
 };
-
 template<size_t state, size_t player>
 struct RowColChecker<state, player, BOARD_SIDE_LENGTH> {
-    enum {value = true};
+    enum {value = false};
 };
 
 //Check if a player won a diagonal
@@ -103,13 +104,14 @@ struct CheckWin {
 //Evaluate a state
 template<size_t state>
 struct BoardEvaluator {
-    static constexpr Evaluation value = (abs((PlayerCounter<state, X_CHAR>::value - PlayerCounter<state, O_CHAR>::value)) > 1
-                                         || (CheckWin<state, X_CHAR>::value && CheckWin<state, O_CHAR>::value)) ? Evaluation::UnreachableState
+    static constexpr Evaluation value = abs((PlayerCounter<state, X_CHAR>::value - PlayerCounter<state, O_CHAR>::value)) > 1
+                                         || (CheckWin<state, X_CHAR>::value && CheckWin<state, O_CHAR>::value) ? Evaluation::UnreachableState
                                                  : (CheckWin<state, X_CHAR>::value ? Evaluation::Xwins
                                                  : (CheckWin<state, O_CHAR>::value ? Evaluation::Owins
                                                  : Evaluation::NoWinner));
 };
 
+//Generate the lookup table
 template<size_t index = 0, Evaluation ...D>
 struct TableGenerator : TableGenerator<index + 1, D..., BoardEvaluator<index>::value> {};
 
@@ -118,11 +120,29 @@ struct TableGenerator<TABLE_SIZE, D...> {
     static constexpr LookupTable table = {D...};
 };
 
+//Contains the evaluation of each table, indexed at the table's
+//ternary equivalent.
 constexpr LookupTable table = TableGenerator<>::table;
 
+//Convert a board into its ternary equivalent
+size_t convert(const std::string &board) {
+    size_t accumulator = 0;
 
-Evaluation EvaluateBoard(const std::string &board_state) {
-    return Evaluation::Xwins;
+    for(size_t i = 0; i < BOARD_AREA; i++) {
+        size_t power = BOARD_AREA - i - 1;
+
+        if(board[i] == 'x' || board[i] == 'X')
+            accumulator += X_CHAR * static_cast<size_t>(std::pow(3, power));
+        else if(board[i] == 'o' || board[i] == 'O')
+            accumulator += O_CHAR * static_cast<size_t>(std::pow(3, power));
+    }
+
+    return accumulator;
 }
 
-// Put other method definitions here
+Evaluation EvaluateBoard(const std::string &board_state) {
+    if(board_state.length() != BOARD_AREA)
+        return Evaluation::InvalidInput;
+
+    return table[convert(board_state)];
+}
