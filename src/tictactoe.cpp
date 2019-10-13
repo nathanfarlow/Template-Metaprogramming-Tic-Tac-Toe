43f4e126713fc2596d26b11170c50f60035de7a6
Nathan Farlow <natethegreatest11@gmail.com>
2019-10-13 00:18:17 -0500

    Remove redundant parentheses

--- src/tictactoe.cpp
+++ src/tictactoe.cpp
@@ -105,9 +105,9 @@ template<size_t state>
 struct BoardEvaluator {
     static constexpr Evaluation value = abs((PlayerCounter<state, X_CHAR>::value - PlayerCounter<state, O_CHAR>::value)) > 1
                                          || (CheckWin<state, X_CHAR>::value && CheckWin<state, O_CHAR>::value) ? Evaluation::UnreachableState
-                                                 : (CheckWin<state, X_CHAR>::value ? Evaluation::Xwins
-                                                 : (CheckWin<state, O_CHAR>::value ? Evaluation::Owins
-                                                 : Evaluation::NoWinner));
+                                                 : CheckWin<state, X_CHAR>::value ? Evaluation::Xwins
+                                                 : CheckWin<state, O_CHAR>::value ? Evaluation::Owins
+                                                 : Evaluation::NoWinner;
 };
 
 //Generate the lookup table
