dbf4a80d6c71aface40a3f287beef9577a861a3e
Nathan Farlow <natethegreatest11@gmail.com>
2019-10-12 20:29:56 -0500

    Move binary files to their own folder

--- compile_main.sh
+++ compile_main.sh
@@ -1,1 +1,1 @@
-/usr/bin/time -v g++ -ftemplate-depth=30000 src/main.cpp src/tictactoe.cpp -o tictactoe 2> main_log.txt
+/usr/bin/time -v g++ -ftemplate-depth=30000 src/main.cpp src/tictactoe.cpp -o bin/tictactoe 2> main_log.txt
--- compile_tests.sh
+++ compile_tests.sh
@@ -1,1 +1,1 @@
-/usr/bin/time -v g++ -ftemplate-depth=30000 src/test.cpp src/tictactoe.cpp -o test 2> test_log.txt
+/usr/bin/time -v g++ -ftemplate-depth=30000 src/test.cpp src/tictactoe.cpp -o bin/test 2> test_log.txt
